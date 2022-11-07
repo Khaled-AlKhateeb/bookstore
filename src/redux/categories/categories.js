f7800bc3636ea629aeb2a8d677acdb79eeef01b7
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2022-11-07 16:28:16 +0200

    edited status to empty array

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,7 +1,7 @@
 const COMPLETED = 'COMPLETED';
 const INPROGRESS = 'INPROGRESS';
 
-const categoryReducer = (state = {}, action) => {
+const categoryReducer = (state = [], action) => {
   switch (action.type) {
     case COMPLETED:
       return 'UNDER CONSTRUCTION';
